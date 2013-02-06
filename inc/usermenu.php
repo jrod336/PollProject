802538968f34c0225c9540cfb2bf45e9587b39b0
Jared Kozel <jrod336@yahoo.com>
2013-02-06 10:26:53 -0700

    User Controller added
    Need to add Login Capabilities next

--- inc/usermenu.php
+++ inc/usermenu.php
@@ -1,5 +1,23 @@
 <?
 	//User Menu Controller
+	class User{
+		public $userid;
+		public $username;
+		
+		//Returns an array of polls for this user
+		public getPolls(){
+			$sql = "select * from `polls` where userid='".$this->$userid."'";
+			$db->query($sql);
+			$polls = array();
+			while($row = $db->nextRow()) {
+				$polls[] = $row['friend_id'];
+			}
+			return $polls;
+		}
+	}
+	
+	$user = new User();
+	$user->userid = 1;	//Temp setting
 	
 	if($_REQUEST['option'] == 'create'){
 		$option = 'create';
